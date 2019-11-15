b718336a188790dc5c1ecbb3fc49762c946c3332
Nevin <nevink01@mountsinai.org>
2019-11-15 09:33:59 -0500

    Updated file selection.

--- Turnaround Time Automation.R
+++ Turnaround Time Automation.R
@@ -7,7 +7,6 @@ library(dplyr)
 library(readxl)
 
 
-test <- choose.files(default = "c:\\*.*", caption = "test")
 # Get and set working directory
 getwd()
 path <- "J:/Presidents/HSPI-PM/Operations Analytics and Optimization/Projects/Service Lines/Lab KPI/Data"
@@ -28,9 +27,11 @@ path_cyto_path <- grep("*Signed Cases Reports$", list.dirs(path), value = TRUE)
 path_cyto_mask <- paste0(path_cyto_path, "/*.*")
 path_cyto_file <- choose.files(default = path_cyto_mask, caption = "Select Today's Pathology & Cytology Report", multi = FALSE)
 path_cyto_raw <- read_excel(path_cyto_file, skip = 1)
+path_cyto_raw <- path_cyto_raw[-nrow(path_cyto_raw), ]
 
 cyto_backlog_path <- grep("*Backlog Reports$", list.dirs(path), value = TRUE)
 cyto_backlog_mask <- paste0(cyto_backlog_path, "/*.*")
 cyto_backlog_file <- choose.files(default = cyto_backlog_mask, caption = "Select Today's Cytology Backlog Report", multi = FALSE)
 cyto_backlog_raw <- read_excel(cyto_backlog_file, skip = 1)
+cyto_backlog_raw <- cyto_backlog_raw[-nrow(cyto_backlog_raw), ]
 
