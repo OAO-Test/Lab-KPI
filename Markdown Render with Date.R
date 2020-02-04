32c697998535341987f7f3f233a2131338912f97
kateenevin <kate.nevin@mssm.edu>
2020-02-04 16:10:54 -0500

    Change output file name for live demo at lab meeting.

--- Markdown Render with Date.R
+++ Markdown Render with Date.R
@@ -1,2 +1,2 @@
-rmarkdown::render("Lab-KPI-Dashboard-RMarkdown-TAT-Merged.Rmd", output_file = paste("Lab KPI Dashboard Weekday Demo", Sys.Date()))
+rmarkdown::render("Lab-KPI-Dashboard-RMarkdown-TAT-Merged.Rmd", output_file = paste("Lab KPI Dashboard Live Demo", Sys.Date()))
 
