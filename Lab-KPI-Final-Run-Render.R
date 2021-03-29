c1cb615873ae2a5f04a7d61532d09d11e670ee39
kateenevin <Kate.Nevin@mssm.edu>
2021-03-29 15:58:04 -0400

    Change path for saving rendered dashboards.

--- Lab-KPI-Final-Run-Render.R
+++ Lab-KPI-Final-Run-Render.R
@@ -1,3 +1,7 @@
 rmarkdown::render("lab_kpi_second_run_dashboard.Rmd", 
-                  output_file = paste("Lab KPI Dashboard Final ",
-                                      format(Sys.Date(), "%m-%d-%y")))
+                  output_file = paste0(
+                    substr(user_directory, 1,
+                           str_locate(user_directory, "/Data")[1] - 1),
+                    "/Dashboard Drafts",
+                    "/Lab KPI Dashboard Final ",
+                    format(Sys.Date(), "%m-%d-%y")))
