322a992e63593038abd3e8630d143f069380c3e3
asalaerekat <asala.erekat@mssm.edu>
2021-03-31 16:29:22 -0400

    Delete cp_subsetting_second_run.R

--- cp_subsetting_second_run.R
+++
@@ -1,9 +0,0 @@
-# Code for subsetting CP historical repository data based on relevant dates ----
-
-# Subset CP data for most recent weekday
-cp_wday_summary <- cp_repo %>%
-  filter(ResultDate %in% wkday_date)
-
-# Subset CP data for weekend/holidays
-cp_not_wday_summary <- cp_repo %>%
-  filter(ResultDate %in% wkend_holiday_date)
