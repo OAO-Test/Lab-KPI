dd5a44ca56fb46042e5c5e82be6fec9d20268277
kateenevin <kate.nevin@mssm.edu>
2021-05-17 12:39:07 -0400

    Create script for gathering all raw data and summarizing for daily, weekly, and monthly repositories.

---
+++ Raw-Data-Summary-Repos/CP_Raw_Data_and_Summary_Repo.R
@@ -0,0 +1,5 @@
+# This code is used to create and maintain a data repository with:
+# 1. Raw data for in scope analytes for 60 days
+# 2. Daily summary based on dashboard stratifications
+# 3. Weekly summary based on dashboard stratifications
+# 4. Monthly summary based on dashboard stratifications
