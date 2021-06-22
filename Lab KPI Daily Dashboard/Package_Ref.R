2e82ff18eb36fac5821156947b1ae105892464ad
kateenevin <kate.nevin@mssm.edu>
2021-06-22 17:01:38 -0400

    Create new scripts for running the dashboard every day of the week, including weekends and holidays.

--- Lab KPI Daily Dashboard/Package_Ref.R
+++ Lab KPI Daily Dashboard/Package_Ref.R
@@ -46,10 +46,14 @@ today <- Sys.Date()
 
 #Determine if yesterday was a holiday/weekend
 #get yesterday's DOW
-yesterday <- today  - 1
+yesterday <- today - 1
 
 #Get yesterday's DOW
 yesterday_day <- wday(yesterday, label = TRUE, abbr = TRUE)
+
+# Format yesterday's date for dashboard visualization
+result_date_text <- format(yesterday,
+                           format = "%a %m/%d/%y")
 
 #Remove Good Friday from MSHS Holidays
 nyse_holidays <- as.Date(holidayNYSE(year = 1990:2100))
